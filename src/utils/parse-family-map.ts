08c1f95b0024f57125f51cd99ad9f3aecfc25188
Lete114 <me@imlete.cn>
2023-01-14 11:04:37 +0800

    chore: 当 font-family 字体声明在 src: url() 之后时无法识别字体的 bug

--- src/utils/parse-family-map.ts
+++ src/utils/parse-family-map.ts
@@ -1,5 +1,6 @@
 import { existsSync, statSync } from 'node:fs'
 import postcss from 'postcss'
+import type { Root } from 'postcss'
 import { Tkv } from '../types'
 import { getQuoteless, getUrls, getAbsolutePath } from '.'
 
@@ -16,48 +17,49 @@ function parseFamilyMap(basePath: string, declaredFamilyMap: Tkv, content: strin
   postcss({
     postcssPlugin: 'fontmin-spider-family-parse',
     Once(root) {
-      root.walkAtRules('font-face', (rule) => {
-        rule.walkDecls('font-family', (decl) => {
-          const family = getQuoteless(decl.value)
-          if (declaredFamilyMap[family]) return
-          declaredFamilyMap[family] = null
-        })
-        rule.walkDecls('src', (decl) => {
-          if (decl.value && decl.value.includes('url(')) {
-            getUrls(decl.value).map((meta) => {
-              const sourcePath = meta.value
-              const filePath = getAbsolutePath(basePath, importPath, sourcePath)
-              if (existsSync(filePath) && statSync(filePath).isFile()) {
-                for (const family in declaredFamilyMap) {
-                  if (declaredFamilyMap[family] === null) {
-                    declaredFamilyMap[family] = {}
-                    declaredFamilyMap[family].selector = []
-                    declaredFamilyMap[family].path = filePath
-                    declaredFamilyMap[family].chars = ''
-                  }
-                }
-              }
-            })
-          }
-        })
-      })
-      root.walkDecls((decl) => {
-        let selector = decl.parent && (decl.parent as Tkv).selector
-        selector = (selector || '').replace(/:?:(active|hover|focus|before|after)/g, '')
-
-        let family = getQuoteless(decl.value)
-        family = family.split(',').find((item) => declaredFamilyMap[getQuoteless(item.trim())]) || ''
-        family = getQuoteless(family)
-
-        if (
-          selector &&
-          decl.prop === 'font-family' &&
-          declaredFamilyMap[family] &&
-          !declaredFamilyMap[family].selector.includes(selector)
-        ) {
-          declaredFamilyMap[family].selector.push(selector)
-        }
-      })
+      parseFontFace(root, basePath, importPath, declaredFamilyMap)
+      parseSelector(root, declaredFamilyMap)
     }
   }).process(content).css
+}
+
+// parse font-face
+function parseFontFace(root: Root, basePath: string, importPath: string, declaredFamilyMap: Tkv) {
+  root.walkAtRules('font-face', (rule) => {
+    let tmpFontName = ''
+    let tmpSourcePath = ''
+    rule.walkDecls(/font-family|src/, (decl) => {
+      if (decl.value && decl.value.includes('url(')) {
+        getUrls(decl.value).forEach((meta) => {
+          const filePath = getAbsolutePath(basePath, importPath, meta.value)
+          if (existsSync(filePath) && statSync(filePath).isFile()) {
+            tmpSourcePath = filePath
+          }
+        })
+      } else {
+        tmpFontName = getQuoteless(decl.value)
+      }
+    })
+    if (tmpFontName && tmpSourcePath) {
+      declaredFamilyMap[tmpFontName] = {}
+      declaredFamilyMap[tmpFontName].selector = []
+      declaredFamilyMap[tmpFontName].path = tmpSourcePath
+      declaredFamilyMap[tmpFontName].chars = ''
+    }
+  })
+}
+
+function parseSelector(root: Root, declaredFamilyMap: Tkv) {
+  root.walkDecls('font-family', (decl) => {
+    let selector = decl.parent && (decl.parent as Tkv).selector
+    selector = (selector || '').replace(/:?:(active|hover|focus|before|after)/g, '')
+
+    let family = getQuoteless(decl.value)
+    family = family.split(',').find((item) => declaredFamilyMap[getQuoteless(item.trim())]) || ''
+    family = getQuoteless(family)
+
+    if (selector && declaredFamilyMap[family] && !declaredFamilyMap[family].selector.includes(selector)) {
+      declaredFamilyMap[family].selector.push(selector)
+    }
+  })
 }
